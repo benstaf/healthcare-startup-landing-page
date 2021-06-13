b8cb21ee9757c513943d07474b249f90fff3665f
Mostafa Benhenda <mostafacebook@gmail.com>
2021-06-13 19:29:21 +0200

    datascience.png

--- components/contents/index.js
+++ components/contents/index.js
@@ -41,7 +41,7 @@ export default () => (
     <article className="providers">
       <div className="providers__draw">
         <img
-          src="./assets/datacenter.jpg"
+          src="./assets/datascience.png"
           alt="illustration"
         />
       </div>
