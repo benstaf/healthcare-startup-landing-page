ac0bc2b06eaa5ad6f9c20b3f1395a11fabc5c8e7
Mostafa Benhenda <mostafacebook@gmail.com>
2021-06-13 15:29:14 +0200

    precision medicine Ching-mi yichue

--- components/services/index.js
+++ components/services/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default () => (
   <section className="services" id="services">
     <article className="services__heading">
-      <h2>The Hub of precision medicine</h2>
+      <h2>The new precision medicine Silk Road</h2>
       <div className="border" />
       <p>
-        ChingHub is a precision medicine ( 精准医学 <i>Ching-mi yichué </i> in Chinese) hub bringing together patient recruitment for clinical trials, data and technologies, and access to private funding.
+        ChingHub is a precision medicine ( 精准医学 <i>Ching-mi yichué </i> in Chinese) hub bringing together access to private funding, data and technologies, and patient recruitment for clinical trials.
       </p> 
     </article>
    
