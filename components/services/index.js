33163507a483a7fb883d5aff6c6ae9aa537a6138
Mostafa Benhenda <mostafacebook@gmail.com>
2021-06-13 23:28:18 +0200

    Global network

--- components/services/index.js
+++ components/services/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => (
   <section className="services" id="services">
     <article className="services__heading">
-      <h2>A global network</h2>
+      <h2>Global network</h2>
       <div className="border" />
       <p>
         ChingHub is a precision medicine ( 精准医学 <i>Ching-mi yichué </i> in Chinese) hub bringing together access to private funding, business partnerships, data and technologies.
