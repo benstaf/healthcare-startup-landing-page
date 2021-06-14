641bdc27d6f230ec28c36fd48e63d383d0c83458
Mostafa Benhenda <mostafacebook@gmail.com>
2021-06-14 14:38:36 +0200

    added offices addresses

--- components/footer/index.js
+++ components/footer/index.js
@@ -4,34 +4,46 @@ import Logo from './logo';
 export default () => (
   <footer className="footer" id="footer">
     <div className="logo-container">
-<img src="./assets/logo_text_chinghub_footer_resized.svg"  />
+
+<p> <b> Our Offices </b> </p>
+
       <p>
-        ChingHub is a global network accelerating precision medicine and immunotherapy research
+2 Dongsan Street, 7/F, Metropolis Tower, Zhongguancun Xi Zone, Beijing, 100080, China
       </p>
-      <p>©ChingHub 2021. All rights reserved</p>
+<p>
+585 Glenwood Ave, Menlo Park, CA 94025, USA
+      </p>
+<p>
+121 Avenue des Champs Elysées, 75008 Paris, France
+      </p>
+
+
+
+
     </div>
    
  <div className="footer__links">
-      
+     
+
  <ul>
         <p>Contact Us</p>
         <li>
-          EMEAA: Mostapha Benhenda <a  href="https://linkedin.com/in/mostapha-benhenda/" target="_blank">(LinkedIn profile)</a>
+          Mostapha Benhenda <a  href="https://linkedin.com/in/mostapha-benhenda/" target="_blank">(LinkedIn)</a>
         </li>
        <li>
-          Asia-Pacific: Vincent Cai <a href="https://www.linkedin.com/in/%E5%8D%9A%E8%94%A1-%E6%99%AE-1885951b3/"  target="_blank">(LinkedIn profile)</a>
+          Vincent Cai <a href="https://www.linkedin.com/in/%E5%8D%9A%E8%94%A1-%E6%99%AE-1885951b3/"  target="_blank">(LinkedIn)</a>
         </li>
      
         <li>
-          General inquiries: <a href="mailto:info@chinghub.com">info@chinghub.com</a>
+          General inquiries: <p> <a href="mailto:info@chinghub.com">info@chinghub.com</a> </p>
         </li>
+
+       
         <li>
-          <a href="https://join.slack.com/t/dsformedicine/shared_invite/zt-kb1mk92u-tQ9FdZ6Iw45W8~p8yJDt4A" target="_blank">Slack</a>
+      ©ChingHub 2021. All rights reserved
         </li>
-        
-        <li>
-          <a href="https://twitter.com/mostafabenh" target="_blank">Twitter</a>
-        </li>
+
+
       </ul>
 
     </div>
