4587301088274dda47e263c1104ecd31c1fe7eef
benstaf <mostafacebook@gmail.com>
2023-04-09 15:39:26 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,17 @@ import {
 export default () => (
   <div>
     <Head>
-      <title>ChingHub - Precision Medicine Network</title>
+      <title>ChingHub - Tech and Medicine Network</title>
 <meta name="image" property="og:image" content="./assets/illustration-2.svg"/>
     <meta
           property="og:description"
-          content="ChingHub is a global network accelerating research and business in Bio and Tech"
+          content="ChingHub is a global network accelerating research and business in Tech and Medicine"
         />
         <meta property="og:image" content="./assets/illustration-2.svg" />
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:site" content="@CoderRocketFuel" />
 <meta name="twitter:title" content="Embed a Twitch Video into a React Website" />
-<meta name="twitter:description" content="ChingHub is a global network accelerating precision medicine and immunotherapy research" />
+<meta name="twitter:description" content="ChingHub is a global network accelerating Tech and Medicine research and business" />
 <meta name="twitter:image" content="./assets/illustration-2.svg" />
     </Head>
     <Navbar />
