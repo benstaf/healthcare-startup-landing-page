d1d60ae42eaa9f4db0c04aa9bd025890ef65dc9e
benstaf <mostafacebook@gmail.com>
2023-04-09 15:33:19 +0200

    Update index.js

--- components/services/index.js
+++ components/services/index.js
@@ -6,7 +6,7 @@ export default () => (
       <h2>Global Network</h2>
       <div className="border" />
       <p>
-ChingHub is a global hub facilitating access to private funding and partnerships, to address global challenges in Bio and Tech 
+ChingHub is a global service facilitating access to private funding and partnerships, to address global challenges in Tech and Medicine 
       </p> 
 
     </article>
