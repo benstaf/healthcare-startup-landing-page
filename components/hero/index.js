bc8d920ceccd3c41ab80bd5d549d140661496cff
benstaf <mostafacebook@gmail.com>
2023-04-09 18:03:31 +0200

    Update index.js

--- components/hero/index.js
+++ components/hero/index.js
@@ -8,10 +8,10 @@ export default () => (
         ChingHub is a global network accelerating Tech and Medicine research and business
       </p>
 
-<a href="https://calendly.com/mostaph/10min"  target="_blank" >
+<a href="https://mostaphabenhenda.typeform.com/to/sHbRUQ9T"  target="_blank" >
 
       <button type="button" className="btn btn-rounded-big">
-        Schedule a Call
+        Sign Up
       </button>
 </a>
 
