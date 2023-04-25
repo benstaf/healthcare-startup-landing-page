80d7749625d482aedb68ab741e1b64657aa73cf8
benstaf <mostafacebook@gmail.com>
2023-04-25 15:52:03 +0200

    Update index.js

--- components/media_coverage/index.js
+++ components/media_coverage/index.js
@@ -9,7 +9,7 @@ export default () => (
     </article>
     <div className="services__cards">
       <div className="services__card">
-        <img src="./assets/fortune.png" alt="Fortune Magazine" />
+        <img src="./assets/fortune.jpg" alt="Fortune Magazine" />
     
       </div>
       <div className="services__card">
