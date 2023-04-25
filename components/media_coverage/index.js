76ffbb110c614d0fceb977e2fe81bec4299984c4
benstaf <mostafacebook@gmail.com>
2023-04-25 17:02:45 +0200

    Update index.js

--- components/media_coverage/index.js
+++ components/media_coverage/index.js
@@ -9,15 +9,20 @@ export default () => (
     </article>
     <div className="services__cards">
       <div className="services__card">
+    <a href="https://fortune.com/longform/ai-artificial-intelligence-medicine-healthcare-pharmaceutical-industry/" target="_blank">
         <img src="./assets/fortune.svg" alt="Fortune Magazine" />
-    
+      </a>
       </div>
       <div className="services__card">
+      <a href="https://www.science.org/content/blog-post/objections-some-drug-discovery-ai" target="_blank">
         <img src="./assets/trace.svg" alt="Science Translational Medicine" />
+      </a>
         
       </div>
       <div className="services__card">
+      <a href="https://blog.benchsci.com/mostapha-benhenda-of-startcrowd-on-ai-drug-discovery-hype" target="_blank">
         <img src="./assets/benchsci.svg" alt="BenchSci" />
+      </a>
      
       </div>
     </div>
