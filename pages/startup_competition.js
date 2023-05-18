a5f31756ab6fa4bf6614d893b4d26cadfe154516
benstaf <mostafacebook@gmail.com>
2023-05-18 15:22:37 +0200

    Update startup_competition.js

--- pages/startup_competition.js
+++ pages/startup_competition.js
@@ -24,7 +24,14 @@ export default () => (
 <meta name="twitter:image" content="./assets/illustration-2.svg" />
     </Head>
     <Navbar />
-  <iframe style={{ textAlign: 'center' }} src="https://docs.google.com/forms/d/e/1FAIpQLScApw9w8dhebGrGoKbCNbLQqjNoKfEMo3CUhrEuSFxnP4ZmpA/viewform?embedded=true" width="640" height="559" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+  <iframe 
+  src="https://docs.google.com/forms/d/e/1FAIpQLScApw9w8dhebGrGoKbCNbLQqjNoKfEMo3CUhrEuSFxnP4ZmpA/viewform?embedded=true" 
+   width="100%"
+        style={{ maxWidth: '640px', width: '100%', height: '559px' }}
+       
+  
+  
+  frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
      <ChatwootWidget />
   </div>
 );
