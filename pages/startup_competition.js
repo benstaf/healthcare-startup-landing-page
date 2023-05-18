8ad76f676d2d881eb676be614666058e908b1fc3
benstaf <mostafacebook@gmail.com>
2023-05-18 14:56:55 +0200

    Create startup_competition.js

---
+++ pages/startup_competition.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Head from 'next/head';
+
+import {
+  Services, Contents, Media_Coverage, Hero, Navbar, Footer, ChatwootWidget, 
+} from '../components';
+
+
+
+export default () => (
+  <div>
+    <Head>
+      <title>ChingHub - Tech and Medicine Network</title>
+<meta name="image" property="og:image" content="./assets/illustration-2.svg"/>
+    <meta
+          property="og:description"
+          content="ChingHub is a global network accelerating research and business in Tech and Medicine"
+        />
+        <meta property="og:image" content="./assets/illustration-2.svg" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@CoderRocketFuel" />
+<meta name="twitter:title" content="Embed a Twitch Video into a React Website" />
+<meta name="twitter:description" content="ChingHub is a global network accelerating Tech and Medicine research and business" />
+<meta name="twitter:image" content="./assets/illustration-2.svg" />
+    </Head>
+    <Navbar />
+    <Hero />
+    <Services />
+    <Contents />
+    <Media_Coverage />
+    <Footer />
+    <ChatwootWidget />
+  </div>
+);
